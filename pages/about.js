a60a751401c149b7333e465812ecbc64f7842373
Slayer <aksuplayz1337@gmail.com>
2021-07-22 21:16:13 +0300

    About Page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import {Button} from "react-bootstrap";
+import {SEOComponent} from "../components/SEO";
+
+export default function AboutMe() {
+  return (
+    <div className="container">
+      <SEOComponent title="About Me"/>
+      <h2>About Me</h2>
+      <p>I&lsquo;m a Full-Stack Developer from Finland. I like coding Discord Bots and Websites. I always make sure the design is perfect on the websites i make.</p>
+      <p>My current goal is to become a truck driver, and deliver stuff around Finland.</p>
+    </div>
+  )
+}
