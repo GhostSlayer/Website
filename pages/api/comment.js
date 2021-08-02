0e0003126ea48d5f843d7ad17c3cf988f132acaa
Slayer <aksuplayz1337@gmail.com>
2021-08-03 01:01:57 +0300

    I'll go to sleep literally i dont remember anything

--- pages/api/comment.js
+++ pages/api/comment.js
@@ -2,7 +2,7 @@ import {sql_query} from "../../lib/mysql"
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const comment = { author: req.body.author, post_slug: req.body.slug, content: req.body.content }
+    const comment = { author: req.body.author, post_slug: req.body.slug, content: req.body.content, avatar: req.body.avatar }
     const commentQuery = await sql_query(`INSERT INTO blog_comments SET ?`, comment);
 
     return res.send(200)
