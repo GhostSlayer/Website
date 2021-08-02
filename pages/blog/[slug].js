ed0646cc250e161ff8c160094a5f2163660e869b
Slayer <aksuplayz1337@gmail.com>
2021-08-03 00:44:28 +0300

    Fix?

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -81,7 +81,7 @@ const Post = ({ post, content, comments }) => {
       <br/>
 
       {comments.map(comment => (
-        <div>
+        <div key={comment.author}>
           <Image className="nav-img" style={{ float: 'left', marginBottom: '6px', marginTop: '-3px' }} width={30} src={session.user.image} alt=""></Image>
           <h5>{comment.author} <small style={{ fontSize: 'x-small' }}>{comment.created_at}</small></h5>
           <p>{comment.content}</p>
